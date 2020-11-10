f6021e835a127547777e05a9ca3936c952e94e0c
ym0179 <ym4766@gmail.com>
2020-11-10 12:00:52 +0900

    day2

--- keras/keras05_train_test.py
+++ keras/keras05_train_test.py
@@ -4,39 +4,33 @@
 import numpy as np 
 
 #1. 데이터
+#학습시킨 데이터로 평가하면 안됨
 x = np.array([1,2,3,4,5,6,7,8,9,10])
 y = np.array([1,2,3,4,5,6,7,8,9,10])
+x_pred = np.array([11,12,13])
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 #2. 모델 구성
 model = Sequential()
-model.add(Dense(30, input_dim=1))
-model.add(Dense(50))
+model.add(Dense(10, input_dim=1))
+model.add(Dense(20))
 model.add(Dense(30))
 model.add(Dense(7))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss="mse", optimizer="adam", 
-            metrics=["mae","acc"]) #평가지표 list로 반환
-#metrics는 훈련에 영향을 끼치지 않음
-#출력값 동일
+            metrics=["mae"]) #metrics는 훈련에 영향을 끼치지 않음
 model.fit(x, y, epochs=100) 
 
 #4. 평가, 예측
 # loss, acc = model.evaluate(x, y) 
-loss = model.evaluate(x, y) #2개 이상의 반환값은 리스트로 반환
+loss = model.evaluate(x, y)
 
 print("loss: ", loss)
 # print("acc: ", acc)
 
-# y_pred = model.predict(x)
-# print("결과물 : \n", y_pred)
-
-
-# 단축키
-# shift+delete 라인 삭제
-# ctrl+/ 라인 주석
-# ctrl+c 라인 복사
+y_pred = model.predict(x_pred)
+print("결과물 : \n", y_pred)
---
+++ keras/keras05_train_test2.py
@@ -0,0 +1,37 @@
+#Day2
+#2020-11-10
+
+import numpy as np 
+
+#1. 데이터
+#학습시킨 데이터로 평가하면 안됨
+x_train = np.array([1,2,3,4,5,6,7,8,9,10]) #훈련할 데이터
+y_train = np.array([1,2,3,4,5,6,7,8,9,10])
+x_test = np.array([11,12,13,14,15]) #평가할 데이터
+y_test = np.array([11,12,13,14,15])
+x_pred = np.array([16,17,18]) #예측할 데이터
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#2. 모델 구성
+model = Sequential()
+model.add(Dense(10, input_dim=1))
+# model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(7))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss="mse", optimizer="adam", 
+            metrics=["mae"])
+model.fit(x_train, y_train, epochs=100) 
+
+#4. 평가, 예측
+# loss, mae = model.evaluate(x, y) 
+loss = model.evaluate(x_test, y_test)
+
+print("loss: ", loss)
+
+y_pred = model.predict(x_pred)
+print("결과물 : \n", y_pred)
