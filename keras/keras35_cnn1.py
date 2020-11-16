11e0589e677605b2ec0550a929975996d9d3f4be
ym0179 <ym4766@gmail.com>
2020-11-16 12:26:05 +0900

    day6

--- keras/keras35_cnn1.py
+++ keras/keras35_cnn1.py
@@ -35,8 +35,39 @@ tf.keras.layers.Conv2D(
 #입력모양: batch_size, timesteps, feature
 #input_shape = (timesteps, feature)
 
-# model.add(Conv2D(10, (2,2), input_shape=(5,5,1))) # output 값: (9x9x10)
-model.add(Conv2D(5, (2,2))) #output 값: (8x8x5)
-model.add(Conv2D(3, (3,3))) #output 값: (6x6x3)
+# model.add(Conv2D(10, (2,2), input_shape=(5,5,1))) # output 값: (9x9x10) 
+model.add(Conv2D(5, (2,2), padding='same')) #output 값: (8x8x5) #padding 넣어줬을 때 output: (9x9x5)
+model.add(Conv2D(3, (3,3), padding='valid')) #output 값: (6x6x3) #padding 안 넣어줬을 때 output: (7x7x3) (input size 9x9x5)
+model.add(Conv2D(7, (2,2))) #output 값: (6x6x7)
 # conv는 layer output에서 차원이 안바뀜 -> 바로 Dense로 못 감
 # LSTM은 두개 엮으면 한 차원 줄어듬 -> 바로 Dense
+
+from tensorflow.keras.layers import MaxPooling2D
+# Maxpolling 2D
+# 이미지 자를 때 **중복하지 않고 최대값 (특징)만 추출 -> 특성이 가장 높은거만 추출
+# 가장 특성치가 높은거만 남기고 데이터를 확 줄여줌
+# 사소한 픽셀의 값을 무시하고, 가장 큰 특징을 나타내는 값을 기록하는 방식
+model.add(MaxPooling2D()) #pool_size default 2 -> output 크기 반으로 줌 -> output 값: (3x3x7) (input size 6x6x3이었음)
+# pool_size: 연산 범위를 의미. 해당 범위 내의 가장 큰 수만을 가져옴
+
+
+'''
+플래튼 레이어(Flatten Layer)
+- CNN에서 컨볼루션 레이어와 풀링 레이어를 반복적으로 거치면 주요 특징만 추출됨 
+- 추출된 주요 특징은 2차원 데이터로 이루어져 있음
+- Dense와 같이 분류를 위한 학습 레이어에서는 1차원 데이터로 바꾸어서 학습 해야함
+tf.keras.layers.Flatten(): 2차원 데이터를 1차원 데이터로 바꾸는 역할의 레이어
+'''
+from tensorflow.keras.layers import Flatten
+model.add(Flatten())
+# input: (3x3x7)
+# output: 3*3*7 = 63 -> (63, )
+model.add(Dense(1))
+
+model.summary()
+# 2 x 2(필터 크기) * 1 (입력 채널(RGB)) * 10(출력 채널) + 10(출력 채널 bias)
+# 2 x 2(필터 크기) * 10 (입력 채널) * 5(출력 채널) + 5(출력 채널 bias)
+# 3 x 3(필터 크기) * 5 (입력 채널) * 3(출력 채널) + 3(출력 채널 bias)
+# 2 x 2(필터 크기) * 3 (입력 채널) * 7(출력 채널) + 7(출력 채널 bias)
+# Dense layer: ( 63(입력 node) + 1(bias) ) * 1(출력 node) = 4
+
