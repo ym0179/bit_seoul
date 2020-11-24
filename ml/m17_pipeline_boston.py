9fb1a3983cbf0603395d0fee25ad5058229d6873
ym0179 <ym4766@gmail.com>
2020-11-24 16:07:34 +0900

    day12

--- ml/m17_pipeline_boston.py
+++ ml/m17_pipeline_boston.py
@@ -25,8 +25,8 @@ x,y = load_boston(return_X_y=True)
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=66, shuffle=True, train_size=0.8)
 
 params = [
-    {'rf__n_estimators' : [200, 300], #결정 트리의 개수, default=10, 많을 수록 좋은 성능이 나올 "수"도 있음 (시간이 오래걸림)
-    'rf__max_depth' : [6, 8, 10, 12], #트리의 깊이, default=None(완벽하게 클래스 값이 결정될 때 까지 분할), 깊이가 깊어지면 과적합될 수 있으므로 적절히 제어 필요
+    {'rf__n_estimators' : [400, 500, 600], #결정 트리의 개수, default=10, 많을 수록 좋은 성능이 나올 "수"도 있음 (시간이 오래걸림)
+    'rf__max_depth' : [6, 8, 10, 12, 14], #트리의 깊이, default=None(완벽하게 클래스 값이 결정될 때 까지 분할), 깊이가 깊어지면 과적합될 수 있으므로 적절히 제어 필요
     'rf__min_samples_leaf' : [3, 5, 7, 10], #리프노드가 되기 위한 최소한의 샘플 데이터 수, default=2, min_samples_split과 함께 과적합 제어 용도
     'rf__min_samples_split' : [2, 3, 5, 10], #노드를 분할하기 위한 최소한의 데이터 수, default=2, 과적합을 제어하는데 사용 (작게 설정할 수록 분할 노드가 많아져 과적합 가능성 증가)
     'rf__n_jobs' : [-1]} #모든 코어를 다 쓰겠다
@@ -43,13 +43,13 @@ print('최적의 매개변수 : ', model.best_estimator_)
 print('최적의 매개변수 : ', model.best_params_)
 
 '''
-r2_score :  0.9193022757450232
+r2_score :  0.9221367786453781
 최적의 매개변수 :  Pipeline(steps=[('scaler', RobustScaler()),      
                 ('rf',
-                 RandomForestRegressor(max_depth=8, min_samples_leaf=3,
-                                       min_samples_split=10, n_jobs=-1))])
-최적의 매개변수 :  {'rf__n_jobs': -1, 'rf__n_estimators': 100, 'rf__min_samples_split': 10, 'rf__min_samples_leaf': 3, 'rf__max_depth': 
-8}
+                 RandomForestRegressor(max_depth=10, min_samples_leaf=3,
+                                       n_estimators=300, n_jobs=-1))])
+최적의 매개변수 :  {'rf__n_jobs': -1, 'rf__n_estimators': 300, 'rf__min_samples_split': 2, 'rf__min_samples_leaf': 3, 'rf__max_depth': 10}
+
 
 r2_score :  0.9187597132816634
 최적의 매개변수 :  Pipeline(steps=[('scaler', StandardScaler()),
--- ml/m17_pipeline_wine.py
+++ ml/m17_pipeline_wine.py
@@ -28,10 +28,10 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=66, shuff
 
 # '모델명__'이 파라미터 key값으로 들어가야함
 params = [
-    {'rf__n_estimators' : [100, 200], #결정 트리의 개수, default=10, 많을 수록 좋은 성능이 나올 "수"도 있음 (시간이 오래걸림)
-    'rf__max_depth' : [6, 8, 10, 12], #트리의 깊이, default=None(완벽하게 클래스 값이 결정될 때 까지 분할), 깊이가 깊어지면 과적합될 수 있으므로 적절히 제어 필요
-    'rf__min_samples_leaf' : [3, 5, 7, 10], #리프노드가 되기 위한 최소한의 샘플 데이터 수, default=2, min_samples_split과 함께 과적합 제어 용도
-    'rf__min_samples_split' : [2, 3, 5, 10], #노드를 분할하기 위한 최소한의 데이터 수, default=2, 과적합을 제어하는데 사용 (작게 설정할 수록 분할 노드가 많아져 과적합 가능성 증가)
+    {'rf__n_estimators' : [400, 500, 600, 700], #결정 트리의 개수, default=10, 많을 수록 좋은 성능이 나올 "수"도 있음 (시간이 오래걸림)
+    'rf__max_depth' : [8, 10, 12, 14, 16], #트리의 깊이, default=None(완벽하게 클래스 값이 결정될 때 까지 분할), 깊이가 깊어지면 과적합될 수 있으므로 적절히 제어 필요
+    'rf__min_samples_leaf' : [5, 7, 10, 12, 14], #리프노드가 되기 위한 최소한의 샘플 데이터 수, default=2, min_samples_split과 함께 과적합 제어 용도
+    'rf__min_samples_split' : [3, 5, 10, 12, 14], #노드를 분할하기 위한 최소한의 데이터 수, default=2, 과적합을 제어하는데 사용 (작게 설정할 수록 분할 노드가 많아져 과적합 가능성 증가)
     'rf__n_jobs' : [-1]} #모든 코어를 다 쓰겠다
 ]
 
@@ -41,17 +41,19 @@ model = RandomizedSearchCV(pipe, params, cv=5, verbose=2)
 
 model.fit(x_train, y_train)
 
-print('acc : ', model.score(x_test,y_test)) #acc :  1.0
 
 print('최적의 매개변수 : ', model.best_estimator_)
 print('최적의 매개변수 : ', model.best_params_)
+print('최고 스코어 : ', model.best_score_)
+print("===========================================")
+print('acc : ', model.score(x_test,y_test))
+
 
 '''
-acc :  0.6795918367346939
+acc :  0.686734693877551
 최적의 매개변수 :  Pipeline(steps=[('scaler', MaxAbsScaler()),      
                 ('rf',
-                 RandomForestClassifier(max_depth=12, min_samples_leaf=3,
-                                        min_samples_split=3, n_estimators=200,
-                                        n_jobs=-1))])
-최적의 매개변수 :  {'rf__n_jobs': -1, 'rf__n_estimators': 200, 'rf__min_samples_split': 3, 'rf__min_samples_leaf': 3, 'rf__max_depth': 12}
+                 RandomForestClassifier(max_depth=14, min_samples_leaf=3,
+                                        n_estimators=500, n_jobs=-1))])
+최적의 매개변수 :  {'rf__n_jobs': -1, 'rf__n_estimators': 500, 'rf__min_samples_split': 2, 'rf__min_samples_leaf': 3, 'rf__max_depth': 14}
 '''
