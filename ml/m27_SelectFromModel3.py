aa44b337f5495848ee22012517fc531d656737f7
ym0179 <ym4766@gmail.com>
2020-11-26 11:37:24 +0900

    day14

--- ml/m27_SelectFromModel3.py
+++ ml/m27_SelectFromModel3.py
@@ -13,14 +13,14 @@
 # 1번값과 2번값 비교
 
 from xgboost import XGBClassifier, XGBRegressor
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV, RandomizedSearchCV
 
-x, y = load_boston(return_X_y=True)
+x, y = load_diabetes(return_X_y=True)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=77)
 
@@ -85,27 +85,24 @@ r2 = model.score(x_test,y_test)
 print("r2 : ", r2)
 
 '''
-최적 하이퍼 파라미터 :  {'n_estimators': 110, 'max_depth': 6, 'learning_rate': 0.1, 'colsample_bytree': 0.6}
-최고 정확도 : 0.8882
-r2 :  0.9088774908941148
+최적 하이퍼 파라미터 :  {'n_estimators': 90, 'max_depth': 4, 'learning_rate': 0.1, 'colsample_bytree': 0.9}
+최고 정확도 : 0.3667
+r2 :  0.45565412996943155
 
-Thresh=0.0012, n=13, R2: 0.8949%
-Thresh=0.0079, n=12, R2: 0.8977%
-Thresh=0.0116, n=11, R2: 0.9033%
-Thresh=0.0170, n=10, R2: 0.9101%
-Thresh=0.0321, n=9, R2: 0.8998%
-Thresh=0.0383, n=8, R2: 0.9002%
-Thresh=0.0417, n=7, R2: 0.8967%
-Thresh=0.0495, n=6, R2: 0.8788%
-Thresh=0.0608, n=5, R2: 0.8697%
-Thresh=0.0652, n=4, R2: 0.8516%
-Thresh=0.1891, n=3, R2: 0.8153%
-Thresh=0.1991, n=2, R2: 0.7389%
-Thresh=0.2864, n=1, R2: 0.4930%
+Thresh=0.0395, n=10, R2: 0.3839%
+Thresh=0.0402, n=9, R2: 0.3455%
+Thresh=0.0474, n=8, R2: 0.3340%
+Thresh=0.0530, n=7, R2: 0.3027%
+Thresh=0.0564, n=6, R2: 0.3421%
+Thresh=0.0721, n=5, R2: 0.3458%
+Thresh=0.0727, n=4, R2: 0.3018%
+Thresh=0.0736, n=3, R2: 0.3432%
+Thresh=0.1887, n=2, R2: 0.2736%
+Thresh=0.3565, n=1, R2: -0.0518%
 
-best_thresh, save_score:  0.01703024 0.9100961288769468
+best_thresh, save_score:  0.039451897 0.3838801478911562
 
-최적 하이퍼 파라미터 :  {'n_estimators': 110, 'max_depth': 4, 'learning_rate': 0.1, 'colsample_bytree': 1, 'colsample_bylevel': 0.7}
-최고 정확도 : 0.8944
-r2 :  0.9222642479401224
+최적 하이퍼 파라미터 :  {'n_estimators': 110, 'max_depth': 5, 'learning_rate': 0.1, 'colsample_bytree': 1, 'colsample_bylevel': 0.7}
+최고 정확도 : 0.3754
+r2 :  0.4733078876221164
 '''
