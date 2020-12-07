925e7546795873e93b3d3420473f6bff90ba6735
ym0179 <ym4766@gmail.com>
2020-12-07 19:44:41 +0900

    day21

--- ml/m32_outliers2.py
+++ ml/m32_outliers2.py
@@ -57,3 +57,24 @@ a = a.transpose()
 outliers2(a)
 
 # print("이상치의 위치 : ",b) #index 값
+
+
+# https://lsjsj92.tistory.com/556
+
+
+'''
+def outliers(data_out) :
+    data_out = data_out.T # 뒤집어서 빼기 편하게
+    del_index = np.array([],dtype=np.int)
+    for cnt in range(data_out.shape[1]):
+        column_data = data_out[:,cnt]
+        quartile_1, quartile_3 = np.percentile(column_data, [25,75]) #데이터의 25%, 75%지점
+        iqr = quartile_3 - quartile_1
+        lower_bound = quartile_1 - (iqr * 1.5)
+        upper_bound = quartile_3 + (iqr * 1.5)
+        outlier_index = np.where((column_data>upper_bound)|(column_data<lower_bound))
+        del_index = np.append(del_index, outlier_index) # 인덱스를 모아서
+    data_out = np.delete(data_out, del_index, axis=0) # 모은 인덱스를 빼라
+    data_out = data_out.T # 다시 뒤집자
+    return data_out
+'''
